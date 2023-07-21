ae35800ceff1bbebfa0a0dfd2a3f7a82d9d47c2d
Akhila Negi <122104289+AkhilaNegi@users.noreply.github.com>
2023-07-21 11:55:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
 //your JS code here. If required.
-function livetime() {
-  let date = new Date();
-  let day = date.getDate();
-  let month = date.getMonth() + 1;
-  let year = date.getFullYear();
-  let time = date.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true });
-  let cudate = `${month}/${day}/${year}`;
-  document.getElementById('timer').textContent = cudate + ', ' + time;
-}
-function startTimer() {
-  livetime();
-  setInterval(livetime, 1000); 
-}
- 
-window.addEventListener('load', startTimer);
+let timer = document.getElementById('timer');
+let newDate =new Date();
+let date = new Date().toLocaleString();
+
+let fullDate = `${newDate.getDate()}/${newDate.getMonth()}/${newDate.getFullYear()}, `
+// let year = newDate.getFullYear()
+timer.innerText = date
